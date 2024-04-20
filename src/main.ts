37b875b4433eef18e3d526cb6934f6280608e09f
Artem Samarin <iahugo@yandex.ru>
2024-04-21 01:29:15 +0300

    Включение CORS и пайпа валидации.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
+import { Logger, ValidationPipe } from '@nestjs/common'
 import { WinstonModule } from 'nest-winston'
 import { NestFactory } from '@nestjs/core'
-import { Logger } from '@nestjs/common'
 
 import { winstonOptions } from '@common/utils/winston'
 import { ConfigService } from '@config/config.service'
@@ -14,6 +14,17 @@ const bootstrap = async () => {
 
   const config = app.get(ConfigService)
   const logger = app.get(Logger)
+
+  app.enableCors({
+    credentials: true,
+    origin: config.gett<string>('ORIGIN'),
+  })
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  )
 
   await app.listen(config.port).finally(() => {
     logger.log(
