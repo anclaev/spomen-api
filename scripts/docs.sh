209885d68ea3715482db6fcfc1b507212b1cde0f
anclaev <iahugo@yandex.ru>
2024-08-03 05:41:27 +0300

    docs: v0.0.5

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-PACKAGE_VERSION=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
 yarn docs
 git add docs
-yarn commit --non-interactive --type=docs --subject="v$PACKAGE_VERSION"
+yarn commit
 yarn push
